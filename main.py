f3c360c41164f9a5bce3357b3c7135062c7e0584
arkyana <arkyana.asmen@gmail.com>
2019-04-13 11:47:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-class App():
-    pass
+from core.baseapp import BaseApp
+
+
+class App(BaseApp):
+    def __init__(self) :
+        BaseApp.__init__(self)
+
+    def on_playing(self):
+        self.check_collision(self.player, self.enemies)
 
 if __name__ == "__main__":
     theApp = App()
